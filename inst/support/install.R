bbef5578b244e1cd5fbe84358c767f5ce3e8e80e
mpjashby <mpjashby@gmail.com>
2021-11-27 18:57:23 +0000

    Add automatic upgrading of dependencies to self-installation script

--- inst/support/install.R
+++ inst/support/install.R
@@ -3,4 +3,8 @@
 
 if (!"remotes" %in% rownames(installed.packages())) install.packages("remotes")
 
-remotes::install_github("mpjashby/learnpop")
+remotes::install_github(
+  repo = "mpjashby/learnpop",
+  # Upgrade all dependencies that are not the latest version
+  upgrade = "always"
+)
